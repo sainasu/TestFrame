e38fbe7bc33b1d15bb26494b3f8f151e6fa513a3
saina_barsud <saina@susainadeMacBook-Pro.local>
2017-11-16 10:31:53 +0800

    随便的

---
+++ TestFrame/TestFrame/Hello.h
@@ -0,0 +1,17 @@
+//
+//  Hello.h
+//  TestFrame
+//
+//  Created by saina_barsud on 2017/11/16.
+//  Copyright © 2017年 saina. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Hello : NSObject
+- (void)hello;
+- (void)byebye;
+
+- (void)byebyeNSObject;
+
+@end
---
+++ TestFrame/TestFrame/Hello.m
@@ -0,0 +1,24 @@
+//
+//  Hello.m
+//  TestFrame
+//
+//  Created by saina_barsud on 2017/11/16.
+//  Copyright © 2017年 saina. All rights reserved.
+//
+
+#import "Hello.h"
+
+@implementation Hello
+- (void)hello{
+        NSLog(@"hello");
+}
+- (void)byebye{
+        NSLog(@"byebye");
+}
+- (void)byebyeNSObject{
+        NSLog(@"byebyeNSObject");
+
+}
+
+
+@end
